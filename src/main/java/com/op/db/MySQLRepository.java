74a8d1597d31b93292c8cd1ad20976d4edc51374
ArunVR <anniyanvr@gmail.com>
2020-12-24 16:46:06 +0530

    initial commit

---
+++ src/main/java/com/op/db/MySQLRepository.java
@@ -0,0 +1,25 @@
+package com.op.db;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Map;
+
+@Repository
+public class MySQLRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public List select(String query) {
+        List<Map<String, Object>> rows = jdbcTemplate.queryForList(query);
+        return rows;
+    }
+
+    public void insertorupdate(String query) {
+        jdbcTemplate.update(query);
+    }
+
+}
